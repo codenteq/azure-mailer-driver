d34f0457b886353b5c173a5655c3f0c875356bb3
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-04-22 09:12:14 +0300

    second commit

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+# Release Instructions
+
+[Releases](https://github.com/codenteq/azure-mailer-driver/releases) are done by [@codenteq](https://github.com/codenteq) for this repository.
---
+++ src/AzureMailerServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Codenteq\Azure\Transport;
+
+use Illuminate\Mail\MailServiceProvider;
+use Symfony\Component\Mailer\Transport\Dsn;
+
+class AzureMailerServiceProvider extends MailServiceProvider
+{
+    /**
+     * Register the Illuminate mailer instance.
+     *
+     * @return void
+     */
+    protected function registerIlluminateMailer()
+    {
+        $this->app->singleton('mail.manager', function($app) {
+            return new AzureMailerManager($app);
+        });
+
+        $this->app->bind('mailer', function ($app) {
+            return $app->make('mail.manager')->mailer();
+        });
+
+        $this->app->extend('azure', function ($app) {
+            return $app->make('mail.manager')->mailer();
+        });
+
+        $this->app->extend('azure', function () {
+            return (new AzureMailerTransportFactory)->create(
+                new Dsn(
+                    'azure+api',
+                    config('mail.mailers.azure.endpoint'),
+                    config('mail.mailers.azure.access_key'),
+                    config('mail.mailers.azure.api_version'),
+                    config('mail.mailers.azure.disable_user_tracking')
+                )
+            );
+        });
+    }
+}
